e01124e9071fef32ea6fcfaa2de0a039f83e78d4
Stefeco <Namgarn@gmail.com>
2021-02-08 08:07:48 +0100

    commit number 2

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,43 +1,36 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.Map;
+
+/**
+ * Date february 7 2021
+ * @author ecste
+ * This is the main method :
+ * 	- call ReadSymptomDataFromFile to read the txt file and list all the symptoms in an ArrayList
+ * 	- call SymptomsManager to read the ArrayList and add all the symptoms in a HasMap. It increments every symptom with new occurences.
+ * 	- call SymptomsOutput to sort the symptoms and their values into a treemap. Then use the FileWriter to print the sorted map on an external file.
+ *
+ */
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
 	
 	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
-
-			line = reader.readLine();	// get another symptom
-		}
+		ReadSymptomDataFromFile readSymptoms = new ReadSymptomDataFromFile("symptoms.txt");
+		ArrayList<String> result = (ArrayList<String>) readSymptoms.GetSymptoms();
 		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
-	}
+		
+		SymptomsManager symptomResult = new SymptomsManager(result);
+		Map<String, Integer> symptomCount = symptomResult.ComputeResults(result);
+
+		
+		SymptomsOutput symptomsFile = new SymptomsOutput();
+		symptomsFile.printSymptoms(symptomCount);
+
+		}
+	
+
+		
+
 }
---
+++ Project02Eclipse/src/com/hemebiotech/analytics/SymptomsOutput.java
@@ -0,0 +1,66 @@
+package com.hemebiotech.analytics;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.TreeMap;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Date : february 8 2021
+ * @author ecste
+ *
+ */
+public class SymptomsOutput {
+	
+	/**
+	 * The constructor for the SymptomsOutput class.
+	 */
+	Map<String, Integer> SymptomsOutput = new HashMap<String, Integer>(){
+		private static final long serialVersionUID = 1L;
+	};
+
+	/**
+	 * printSymptoms : Method that sorts alphabetically the result of the symptoms into a treemap and prints it
+	 * then use the writer method to export the result in the result.out file.
+	 * 
+	 * @param symptoms is the HashMap created by the SymptomsManager class.
+	 */
+	public Map<String, Integer> printSymptoms(Map<String, Integer> symptoms) {
+	
+		Map<String, Integer> Symptoms = new TreeMap<String, Integer>();
+		
+		FileWriter writer;
+		/**
+		 * The try catch returns an error if the file is missing or if the file is of the wrong type.
+		 */
+		try {
+			writer = new FileWriter ("result.out");
+			/**
+			 * It is necessary to add a constructor and change the type of sortedSymptoms into a treemap.
+			 * So we are able to sort the indexes alphabetically.
+			 */
+			Map<String, Integer> sortedSymptoms = new TreeMap<String, Integer>(symptoms);
+			
+			/**
+			 * We run an iteration on sortedSymptoms to print them to an external file.
+			 */
+			for(Map.Entry<String, Integer> entry:sortedSymptoms.entrySet()) {
+				
+				System.out.println("sorted values : " + entry.getKey() + " " + entry.getValue());
+				writer.write("sorted values : " + entry.getKey() + " " + entry.getValue());
+				writer.write("\r\n");
+			}
+
+			writer.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return Symptoms;
+		
+	}
+
+}
+
+
